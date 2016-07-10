8b2fca950a1616ed8e89447bf36676e3716a450c
Hasan Karahan <hasan.karahan@blackhan.com>
2016-07-10 18:59:42 +0400

    DIG-35: w/tslint:recommended

--- generators/ext-type-script/templates/src/app/window.ts
+++ generators/ext-type-script/templates/src/app/window.ts
@@ -1,11 +1,11 @@
-declare let window: Window;
+declare let window: IWindow;
 export default window;
 
-export interface Window {
+export interface IWindow {
     showBack: Function;
     showFront: Function;
 }
 
-export interface Window {
+export interface IWindow {
     i18n: Function;
 }
