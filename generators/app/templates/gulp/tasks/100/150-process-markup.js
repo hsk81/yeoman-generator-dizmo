3a07c3c4a74350b690c922d95cff1df3167cfde6
Hasan Karahan <hasan.karahan@blackhan.com>
2017-04-25 12:48:01 +0300

    DIG-98: htmlmin w/CLI option

--- generators/app/templates/gulp/tasks/100/150-process-markup.js
+++ generators/app/templates/gulp/tasks/100/150-process-markup.js
@@ -1,37 +1,52 @@
 var pkg = require('../../package.js'),
-    path = require('path');
-var gulp = require('gulp'),
-    gulp_htmlmin = require('gulp-htmlmin'),
+    path = require('path'),
     extend = require('xtend');
+var gulp = require('gulp'),
+    gulp_htmlmin = require('gulp-htmlmin');
 
 gulp.task('process-markup', function () {
-    var htmlmin = {
-        collapseWhitespace: false,
-        minifyCSS: false,
-        minifyJS: false,
-        removeComments: false
-    };
+    var cli_min = require('yargs')
+        .default('minify')
+        .argv.minify;
+
+    var htmlmin = cli_min === true;
 
     if (pkg.dizmo && pkg.dizmo.build) {
-        var min = pkg.dizmo.build.minify;
-        if (min) {
-            var markups = min.markups !== undefined ? min.markups : {};
-            if (markups) {
-                if (markups.htmlmin || markups.htmlmin === undefined) {
+        var cfg_min = pkg.dizmo.build.minify;
+        if (cfg_min) {
+            var cfg_ms = cfg_min.markups !== undefined ? cfg_min.markups : {};
+            if (cfg_ms) {
+                if (cli_min === undefined && (
+                    cfg_ms.htmlmin || cfg_ms.htmlmin === undefined))
+                {
                     htmlmin = extend({
                         collapseWhitespace: true,
                         minifyCSS: true,
                         minifyJS: true,
                         removeComments: true
-                    }, markups.htmlmin);
+                    }, cfg_ms.htmlmin);
                 }
             }
         }
     }
 
+    var argv = require('yargs')
+        .default('htmlmin', htmlmin).argv;
+
+    if (typeof argv.htmlmin === 'string') {
+        argv.htmlmin = JSON.parse(argv.htmlmin);
+    }
+
     var bundle = gulp.src('src/**/*.html');
-    if (htmlmin) {
-        bundle = bundle.pipe(gulp_htmlmin.apply(this, [htmlmin]));
+    if (argv.htmlmin) {
+        bundle = bundle.pipe(gulp_htmlmin.apply(
+            this, [extend({
+                collapseWhitespace: true,
+                minifyCSS: true,
+                minifyJS: true,
+                removeComments: true
+            }, argv.htmlmin)]
+        ));
     }
     return bundle
         .pipe(gulp.dest(path.join('build', pkg.name)));
