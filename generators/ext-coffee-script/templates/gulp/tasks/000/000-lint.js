efe959213fe8c3b0b157f281e854af3fff4a1c36
Hasan Karahan <hasan.karahan@blackhan.com>
2017-04-26 16:30:15 +0300

    DIG-101: CoffeeScript w/linting via CLI
    It's possible to run the CoffeeScript sub-generator w/linting, which can now
    also be explicitly controlled via the CLI.
    
    Make (by default w/linting via `package.json`):
    ```
    npm run make
    ```
    
    Make w/explicit linting:
    ```
    npm run make -- --lint
    ```
    
    Make w/linting explictly disabled:
    ```
    npm run make -- --no-lint
    ```
    
    Make w/explicit linting, incl. a special rule override:
    ```
    npm run make -- --lint='{"indentation":{"value":3,"level":"error"}}'
    ```
    
    The same CLI argument(s) can also be provided when working with the `npm run
    lint` script.

--- generators/ext-coffee-script/templates/gulp/tasks/000/000-lint.js
+++ generators/ext-coffee-script/templates/gulp/tasks/000/000-lint.js
@@ -1,24 +1,38 @@
 var pkg = require('../../package.js'),
-    gulp = require('gulp'),
+    extend = require('xtend');
+var gulp = require('gulp'),
     gulp_coffeelint = require('gulp-coffeelint');
 
 gulp.task('lint:coffee', function () {
-    var coffeelint;
+    var lint = true;
     if (pkg.dizmo && pkg.dizmo.build) {
-        var build = pkg.dizmo.build;
-        if (build.lint !== null && typeof build.lint === 'object') {
-            coffeelint = build.lint;
-        } else if (build.lint === false) {
-            coffeelint = false;
+        var cfg_lint = pkg.dizmo.build.lint;
+        if (cfg_lint || cfg_lint === undefined) {
+            lint = extend({}, cfg_lint);
+        } else {
+            lint = false;
         }
     }
-    if (coffeelint || coffeelint === undefined) {
-        return gulp.src([
-            './src/**/*.coffee', '!src/lib/**', '!build/**', '!node_modules/**'
-        ])
-        .pipe(gulp_coffeelint.apply(this, coffeelint))
-        .pipe(gulp_coffeelint.reporter())
+
+    var argv = require('yargs')
+        .default('lint', lint).argv;
+    if (typeof argv.lint === 'string') {
+        argv.lint = JSON.parse(argv.lint);
     }
+
+    var bundle = gulp.src([
+        './src/**/*.coffee', '!src/lib/**', '!build/**', '!node_modules/**']);
+    if (argv.lint || argv.lint === undefined) {
+        if (Object.keys(argv.lint).length > 0) {
+            bundle = bundle.pipe(gulp_coffeelint.apply(this, [
+                extend({}, argv.lint)
+            ]));
+        } else {
+            bundle = bundle.pipe(gulp_coffeelint.apply(this));
+        }
+        bundle = bundle.pipe(gulp_coffeelint.reporter());
+    }
+    return bundle;
 });
 
 gulp.task('lint', ['lint:coffee']);
