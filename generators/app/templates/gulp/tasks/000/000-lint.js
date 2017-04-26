5b6d3b8e67c3531286d66ec702a8f12283acdfb3
Hasan Karahan <hasan.karahan@blackhan.com>
2017-04-26 12:32:56 +0300

    DIG-79: Linting via CLI `--lint` option
    It's now possible to override the default linting configuration in
    `.eslintrc.json` by providing a corresponding `--lint` flag on the CLI.
    
    Make w/explicit linting:
    
    ```
    npm run make -- --lint
    ```
    
    Make w/linting suppressed:
    
    ```
    npm run make -- --no-lint
    ```
    
    Make w/linting via specific configuration; e.g. below the `no-unused-vars` rule
    is disabled, which is then *added* to the current set of rules specified in in
    the configuration file `.eslintrs.json`:
    ```
    npm run make -- --lint='{"rules":{"no-unused-vars":0}}'
    ```
    
    Make w/linting via specific configuration; enabled the `no-unused-vars` rule as a
    warning:
    
    ```
    npm run make -- --lint='{"rules":{"no-unused-vars":1}}'
    ```
    
    Make w/linting via specific configuration; enabled the `no-unused-vars` rule as
    an error:
    
    ```
    npm run make -- --lint='{"rules":{"no-unused-vars":2}}'
    ```
    
    In the context of dizmoGen, there is no real difference between a warning and an
    error, since in the latter case the build *still* goes through! The corresponding
    linting message is simply shown as an error w/o stopping the build process.
    
    In cases, where the build process shall explicitly be stopped on an error the
    following code should be added to the project specific `gulp/tasks/000-lint.js`
    script:
    
    ```js
    if (argv.lint || argv.lint === undefined) {
        bundle = bundle.pipe(gulp_eslint.apply(
            this, [extend({}, argv.lint)]
        ))
        .pipe(gulp_eslint.format())
        .pipe(eslint.failAfterError()); // exits!
    }
    ```

--- generators/app/templates/gulp/tasks/000/000-lint.js
+++ generators/app/templates/gulp/tasks/000/000-lint.js
@@ -1,23 +1,32 @@
 var pkg = require('../../package.js'),
-    gulp = require('gulp'),
+    extend = require('xtend');
+var gulp = require('gulp'),
     gulp_eslint = require('gulp-eslint');
 
 gulp.task('lint:js', function () {
-    var eslint;
+    var eslint = true;
+
     if (pkg.dizmo && pkg.dizmo.build) {
-        var build = pkg.dizmo.build;
-        if (build.lint !== null && typeof build.lint === 'object') {
-            eslint = build.lint;
-        } else if (build.lint === false) {
-            eslint = false;
+        var cfg_lint = pkg.dizmo.build.lint;
+        if (cfg_lint || cfg_lint === undefined) {
+            eslint = extend({}, cfg_lint);
         }
     }
-    if (eslint || eslint === undefined) {
-        return gulp.src([
-            './src/**/*.js', '!src/lib/**', '!build/**', '!node_modules/**'])
-            .pipe(gulp_eslint.apply(this, eslint))
-            .pipe(gulp_eslint.format());
+
+    var argv = require('yargs')
+        .default('lint', eslint).argv;
+    if (typeof argv.lint === 'string') {
+        argv.lint = JSON.parse(argv.lint);
     }
+
+    var bundle = gulp.src([
+        './src/**/*.js', '!src/lib/**', '!build/**', '!node_modules/**']);
+    if (argv.lint || argv.lint === undefined) {
+        bundle = bundle.pipe(gulp_eslint.apply(
+            this, [extend({}, argv.lint)]
+        )).pipe(gulp_eslint.format());
+    }
+    return bundle;
 });
 
 gulp.task('lint', ['lint:js']);
