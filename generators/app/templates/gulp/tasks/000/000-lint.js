7569ffe83972aeaebeae3b6d22755357f1c00417
Hasan Karahan <hasan.karahan@blackhan.com>
2017-04-26 16:16:30 +0300

    DIG-79: fixed lint:false case

--- generators/app/templates/gulp/tasks/000/000-lint.js
+++ generators/app/templates/gulp/tasks/000/000-lint.js
@@ -4,17 +4,18 @@ var gulp = require('gulp'),
     gulp_eslint = require('gulp-eslint');
 
 gulp.task('lint:js', function () {
-    var eslint = true;
-
+    var lint = true;
     if (pkg.dizmo && pkg.dizmo.build) {
         var cfg_lint = pkg.dizmo.build.lint;
         if (cfg_lint || cfg_lint === undefined) {
-            eslint = extend({}, cfg_lint);
+            lint = extend({}, cfg_lint);
+        } else {
+            lint = false;
         }
     }
 
     var argv = require('yargs')
-        .default('lint', eslint).argv;
+        .default('lint', lint).argv;
     if (typeof argv.lint === 'string') {
         argv.lint = JSON.parse(argv.lint);
     }
@@ -22,9 +23,9 @@ gulp.task('lint:js', function () {
     var bundle = gulp.src([
         './src/**/*.js', '!src/lib/**', '!build/**', '!node_modules/**']);
     if (argv.lint || argv.lint === undefined) {
-        bundle = bundle.pipe(gulp_eslint.apply(
-            this, [extend({}, argv.lint)]
-        )).pipe(gulp_eslint.format());
+        bundle = bundle
+            .pipe(gulp_eslint.apply(this, [extend({}, argv.lint)]))
+            .pipe(gulp_eslint.format());
     }
     return bundle;
 });
