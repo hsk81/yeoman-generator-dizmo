25fe07667bc80e906936855202e26a4506ed598f
Hasan Karahan <hasan.karahan@blackhan.com>
2017-05-10 12:52:00 +0300

    DIG-70: Run `npm install` upon running a script
    Running NPM scripts like `make`, `lint`, `clean` etc. requires the (dev-)
    dependencies to be installed by running `npm install`. However, many new
    developers to the NPM ecosystem don't know that.
    
    Hence, a *heuristic* has been introduced, where upon running an NPM script the
    existence of the `node_modules` directory is checked:
    
    * If it does not exist, then  `npm install` is invoked (and then the actual
      script itself).
    
    * But if the directory does exist, then it is *assumed* that the content of
      `node_modules` is *correct* and the script is executed directly, *w/o* running
      `npm install` (and hence saving time).

---
+++ generators/app/templates/gulp/scripts/make.js
@@ -0,0 +1,23 @@
+var child_process = require('child_process'),
+    fs = require('fs');
+
+var run_script = function () {
+    child_process.spawn('node', [
+        './node_modules/gulp/bin/gulp.js'
+    ].concat(process.argv.slice(2)), {
+        stdio: 'inherit'
+    });
+};
+
+fs.access('./node_modules', function (error) {
+    if (error) {
+        var npm_install = child_process.spawn('npm', [
+            'install'
+        ], {
+            stdio: 'inherit'
+        });
+        npm_install.on('close', run_script);
+    } else {
+        run_script();
+    }
+});
