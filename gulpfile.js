774711ab892c34119fc5d9d69094dea4e70c3d9e
Hasan Karahan <hasan.karahan@blackhan.com>
2018-08-06 16:46:09 +0300

    DIG-196: w/"gulp": "^4.0.0"

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,24 @@
 'use strict';
+let path = require('path'),
+    pump = require('pump');
+let gulp = require('gulp'),
+    gulp_eslint = require('gulp-eslint'),
+    gulp_exclude = require('gulp-exclude-gitignore'),
+    gulp_nsp = require('gulp-nsp');
 
 let os = require('os');
 if (os.tmpdir) {
     os.tmpDir = os.tmpdir;
 }
 
-let path = require('path'),
-    pump = require('pump');
-let gulp = require('gulp'),
-    gulp_eslint = require('gulp-eslint'),
-    gulp_ex_gitignore = require('gulp-exclude-gitignore'),
-    gulp_nsp = require('gulp-nsp');
+gulp.task('prepare', done => { gulp_nsp({
+    package: path.resolve('package.json')
+}, done); });
 
-gulp.task('static', function (cb) {
-    pump([
-        gulp.src('**/*.js'),
-        gulp_ex_gitignore(),
-        gulp_eslint(),
-        gulp_eslint.format(),
-        gulp_eslint.failAfterError()
-    ], cb);
-});
-
-gulp.task('nsp', function (cb) {
-    gulp_nsp({package: path.resolve('package.json')}, cb);
-});
-
-gulp.task('prepublish', ['nsp']);
-gulp.task('default', ['static']);
+gulp.task('default', done => { pump([
+    gulp.src('**/*.js'),
+    gulp_exclude(),
+    gulp_eslint(),
+    gulp_eslint.format(),
+    gulp_eslint.failAfterError()
+], done); });
