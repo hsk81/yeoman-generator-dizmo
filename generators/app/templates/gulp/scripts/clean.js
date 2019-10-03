c78495d1f7bfd55289308507b56242b6b124465a
Hasan Karahan <hasan.karahan@blackhan.com>
2019-10-03 17:13:41 +0300

    DIG-267: fixed missing semi-colons

--- generators/app/templates/gulp/scripts/clean.js
+++ generators/app/templates/gulp/scripts/clean.js
@@ -5,7 +5,7 @@ const run_script = () => {
     const args = ['./node_modules/gulp/bin/gulp.js', 'clean'];
     args.push(...process.argv.slice(2));
     const run = cli.run('node', ...args)();
-    run.catch(process.exit)
+    run.catch(process.exit);
 };
 
 fs.access('./node_modules').then(run_script).catch(() => {
