0483b58448d466f6636a71366df21dffff7b668d
Hasan Karahan <hasan.karahan@blackhan.com>
2019-10-08 17:50:40 +0300

    DIG-280: fixed generator tests

--- test/test-app.js
+++ test/test-app.js
@@ -162,6 +162,7 @@ describe('generator-dizmo:app', function () {
                 },
                 'optionalDependencies': {
                     'pump': '^3.0.0',
+                    'terser-webpack-plugin': '^2.1.2',
                     'webpack-obfuscator': '^0.18.3'
                 }
             });
