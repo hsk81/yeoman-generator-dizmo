1a709717d16fd09a9c5d18e0d89a4df0830e994f
Hasan Karahan <hasan.karahan@blackhan.com>
2020-05-27 00:21:42 +0300

    DIG-320: build w/elements-version => index.html

--- generators/app/templates/gulp/tasks/build/markup/index.js
+++ generators/app/templates/gulp/tasks/build/markup/index.js
@@ -6,10 +6,17 @@ const path = require('path');
 
 const api_version = pkg.dizmo &&
     pkg.dizmo.settings && pkg.dizmo.settings['api-version'];
+const elm_version = pkg.dizmo &&
+    pkg.dizmo.settings && pkg.dizmo.settings['elements-version'];
+
 const rgx_script = (path) => new RegExp(
     `<script([\\s\\S]+)src="/scripts/${path}-([\\.\\d]+).js"([\\s\\S]*)>([\\s\\S]*)</script>`);
 const tag_script = (path, ...g) =>
     `<script${g[0]}src="/scripts/${path}-${g[1]}.js"${g[2]}>${g[3]}</script>`;
+const rgx_style = (path) => new RegExp(
+    `<link([\\s\\S]+)href="/styles/${path}-([\\.\\d]+).css"([\\s\\S]*)>`);
+const tag_style = (path, ...g) =>
+    `<link${g[0]}href="/styles/${path}-${g[1]}.css"${g[2]}>`;
 
 gulp.task('markup', () => {
     const minify = require('yargs')
@@ -21,6 +28,14 @@ gulp.task('markup', () => {
         .pipe(gulp_replace(rgx_script('dizmojs'), (match, ...groups) => {
             if (api_version) groups[1] = api_version;
             return tag_script('dizmojs', ...groups);
+        }))
+        .pipe(gulp_replace(rgx_script('dizmoelements'), (match, ...groups) => {
+            if (elm_version) groups[1] = elm_version;
+            return tag_script('dizmoelements', ...groups);
+        }))
+        .pipe(gulp_replace(rgx_style('dizmoelements'), (match, ...groups) => {
+            if (elm_version) groups[1] = elm_version;
+            return tag_style('dizmoelements', ...groups);
         }));
     if (typeof argv.htmlmin === 'string') {
         argv.htmlmin = JSON.parse(argv.htmlmin);
